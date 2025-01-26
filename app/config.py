3c6528034647f2ec4d382d475dcd13a2faea002f
Sarah <sarahunoke@gmail.com>
2025-01-26 18:34:32 +0100

    added cors

--- app/config.py
+++ app/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     SECRET_KEY:str = os.environ.get('SECRET_KEY')
     ACCESS_TOKEN_EXPIRE_MINUTES:str = '60'
     ALGORITHM:str = 'HS256'
-    BACKEND_CORS_ORIGINS: List[str] = ['http://localhost:5173']
+    BACKEND_CORS_ORIGINS: List[str] = ['http://localhost:5173','https://personal-resource-tracker-app.onrender.com/']
     TIME_ZONE:str = 'Africa/Lagos'
 
     # @validator("BACKEND_CORS_ORIGINS", pre=True)
